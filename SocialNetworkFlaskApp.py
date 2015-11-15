96147060843a4300c6a9d916aabbd9ee71702e96
James Tench <tenchjames@gmail.com>
2015-11-15 16:11:53 -0500

    added logout

--- SocialNetworkFlaskApp.py
+++ SocialNetworkFlaskApp.py
@@ -1,7 +1,8 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 
 from flask.ext.bcrypt import check_password_hash
-from flask.ext.login import LoginManager, login_user
+from flask.ext.login import (LoginManager, login_user, logout_user,
+                             login_required)
 
 import models
 import forms
@@ -75,6 +76,14 @@ def login():
             else:
                 flash("Your email or password doesn't match!", "error")
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out! Come back soon!", "success")
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
