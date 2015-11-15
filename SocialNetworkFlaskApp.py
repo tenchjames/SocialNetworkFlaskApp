563d3c6aca8aa92b9e797a5799a8d47499da27b7
James Tench <tenchjames@gmail.com>
2015-11-15 13:49:14 -0500

    @before and @after requests added

--- SocialNetworkFlaskApp.py
+++ SocialNetworkFlaskApp.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, g
+
+import models
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the db connection after each request"""
+    g.db.close()
+    return response
 
 
 @app.route('/')
@@ -9,4 +27,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=DEBUG, host=HOST, port=PORT)
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 import datetime
 
 from flask.ext.login import UserMixin
+from flask.ext.bcrypt import generate_password_hash
+from flask.ext.bcrypt import check_password_hash
 from peewee import *
 
 DATABASE = SqliteDatabase('social.db')
 
-class User(Model):
+class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
@@ -15,3 +17,15 @@ class User(Model):
     class Meta:
         database = DATABASE
         order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin
+            )
+        except IntegrityError:
+            raise ValueError("User already exists")
